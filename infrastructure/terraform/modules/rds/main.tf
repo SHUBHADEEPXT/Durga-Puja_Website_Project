e53b2ce177e158ce77f0d50ebabbed63c5752d6d
SHUBHADEEPXT <shubhadeep010@gmail.com>
2025-10-07 16:31:11 +0000

    feat: Modified the Rds version as it is not available in the region where we are creating the infrastructure

--- infrastructure/terraform/modules/rds/main.tf
+++ infrastructure/terraform/modules/rds/main.tf
@@ -39,7 +39,7 @@ resource "aws_security_group" "rds" {
 resource "aws_db_instance" "main" {
   identifier             = "${var.project_name}-db"
   engine                 = "postgres"
-  engine_version         = "15.4"
+  engine_version         = "15"
   instance_class         = "db.t3.micro"
   allocated_storage      = 20
   max_allocated_storage  = 100
